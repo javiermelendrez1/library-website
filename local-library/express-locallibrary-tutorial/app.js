37517efdb01d091fe74b37a46c80e1dae4b2bb69
Javier Melendrez <81501034+javiermelendrez1@users.noreply.github.com>
2021-08-18 14:42:19 -0700

    create connection to MongoDB

--- local-library/express-locallibrary-tutorial/app.js
+++ local-library/express-locallibrary-tutorial/app.js
@@ -1,5 +1,13 @@
 var createError = require('http-errors');
 var express = require('express');
+//connect to MongoDB
+//Set up mongoose connection
+var mongoose = require('mongoose');
+var mongoDB = 'mongodb+srv://javim1224:Monterey96@cluster0.lesqs.mongodb.net/local_library?retryWrites=true&w=majority';
+mongoose.connect(mongoDB, { useNewUrlParser: true , useUnifiedTopology: true});
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
